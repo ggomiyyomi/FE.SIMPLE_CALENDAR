f9987f39e997962dbc34626a91c22c06fe2be08d
HeeJeongSeo <heejung9965@naver.com>
2024-12-13 18:39:38 +0900

    달력디자인& 현재년월 업뎃

---
+++ script.js
@@ -0,0 +1,8 @@
+const currentDate = document.querySelector(".current-date");
+
+// 현재 year과 month 업데이트 
+let date = new Date();
+currYear = date.getFullYear();
+currMonth = date.getMonth();
+
+console.log(date,currYear,currMonth);
